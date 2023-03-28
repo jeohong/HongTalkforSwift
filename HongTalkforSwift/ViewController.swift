c286d529601644641a894df4d6a560121ee07251
HongJeongmin <jeohong59@etomato.com>
2023-03-28 16:35:52 +0900

    Initial Commit

---
+++ HongTalkforSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HongTalkforSwift
+//
+//  Created by 홍정민 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
