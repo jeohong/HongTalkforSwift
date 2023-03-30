60d96f1f3eee7130ccf4b370bcf4ac1fb181a2ed
jeohong <jeohong59@gmail.com>
2023-03-30 13:41:05 +0900

    [FEAT] 채팅방 리스트 뷰 구현

--- HongTalkforSwift/TabBar/ChatRoomsViewController.swift
+++ HongTalkforSwift/TabBar/ChatRoomsViewController.swift
@@ -21,9 +21,14 @@ class ChatRoomsViewController: UIViewController {
         self.getChatroomsList()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        viewDidLoad()
+    }
+    
     func getChatroomsList() {
         Database.database().reference().child("chatrooms").queryOrdered(byChild: "users/"+uid).queryEqual(toValue: true).observeSingleEvent(of: .value) { datasnapshot in
             for item in datasnapshot.children.allObjects as! [DataSnapshot] {
+                self.chatrooms.removeAll()
                 if let chatroomdic = item.value as? [String:AnyObject] {
                     let chatmodel = ChatModel(JSON: chatroomdic)
                     self.chatrooms.append(chatmodel!)
@@ -46,10 +51,42 @@ extension ChatRoomsViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RowCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RowCell", for: indexPath) as! CustomCell
+        
+        var destinationUid: String?
+        
+        for item in chatrooms[indexPath.row].users {
+            if (item.key != self.uid) {
+                destinationUid = item.key
+            }
+        }
+        
+        Database.database().reference().child("users").child(destinationUid!).observeSingleEvent(of: .value) { datasnapshot in
+            let userModel = UserModel()
+            userModel.setValuesForKeys(datasnapshot.value as! [String:AnyObject])
+            
+            cell.label_title.text = userModel.userName
+            let url = URL(string: userModel.profileImageUrl!)
+            URLSession.shared.dataTask(with: url!) { data, response, error in
+                
+                DispatchQueue.main.async {
+                    cell.imageview.image = UIImage(data: data!)
+                    cell.imageview.layer.cornerRadius = cell.imageview.frame.width / 2
+                    cell.imageview.layer.masksToBounds = true
+                }
+            }.resume()
+            
+            let lastMessageKey = self.chatrooms[indexPath.row].comments.keys.sorted(){ $0 > $1 }
+            cell.label_lastMessage.text = self.chatrooms[indexPath.row].comments[lastMessageKey[0]]?.message
+        }
         
         return cell
     }
-    
+}
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var label_title: UILabel!
+    @IBOutlet weak var label_lastMessage: UILabel!
     
 }
